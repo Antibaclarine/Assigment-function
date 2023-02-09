0e28cfe51f1b07d40f7a18a2226e20d8d99f2c2d
clarine <antibaclarine.com>
2023-02-09 07:11:30 +0300

    function

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,29 @@
+fun main() {
+    println("Hello Clarine")
+add2(8,7,3,9)
+fact()
+
+}
+fun name(){
+    println("Hello Clarine")
+
+}
+fun add(){
+    var x = 43
+    var y =5
+    var sum =x % y
+
+println(sum)
+
+}
+fun add2(y: Int , x: Int , a:Int ,b: Int){
+var sum=y + x + a + b
+println(sum)
+}
+fun fact(){
+    var fact="i love coding"
+    println(fact)
+}
+
+
+
